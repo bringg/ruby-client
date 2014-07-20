95e7e6830fa7e2e413c540175ccd2e719f4114e7
Lior Sion <lior.sion@gmail.com>
2014-07-20 16:21:45 +0300

    updates and add delete task method:

--- bringg_client.rb
+++ bringg_client.rb
@@ -10,7 +10,7 @@
     @auth_token = options[:auth_token]
     @hmac_secret = options[:hmac_secret]
     @default_url = options[:url]
-    @default_url ||= "http://api.overvyoo.com"
+    @default_url ||= "http://api.bringg.com"
   end
 
   def create_customer(customer_details)
@@ -21,8 +21,11 @@
 
   def create_task(task_deails)
     uri = URI("#{@default_url}/partner_api/tasks")
-    params = task_deails
-    Net::HTTP.post_form(uri, sign_request(params))
+    req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
+    req.body = sign_request(task_deails).to_json
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
   end
 
   def add_waypoint(task_id, way_point_details)
@@ -31,14 +34,27 @@
     Net::HTTP.post_form(uri, sign_request(params))
   end
 
-  private
+  def delete_task(id)
+    params = sign_request({})
+    query_params = params.collect do |key,val|
+      "#{CGI.escape(key.to_s)}=#{CGI.escape(params[key].to_s)}"
+    end * '&'
+    uri = URI("#{@default_url}/partner_api/tasks/#{id}?#{query_params}")
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+        request = Net::HTTP::Delete.new uri
+        response = http.request request
+    end
+  end
+
+  # private
   def sign_request(params)
      params[:timestamp] ||= Time.now.to_i
      params[:access_token] ||= @auth_token
      query_params = params.collect do |key,val|
        "#{CGI.escape(key.to_s)}=#{CGI.escape(params[key].to_s)}"
      end * '&'
-     puts "signing #{query_params}"
+     puts "signing #{query_params} with #{@hmac_secret}"
      params.merge(signature: OpenSSL::HMAC.hexdigest("sha1", @hmac_secret, query_params))
   end
  end
